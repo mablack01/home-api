4ac583ebf3f3aafba8d010e69909ebe9278e439f
Miles Black <mablack15@gmail.com>
2021-12-05 16:35:15 -0500

    Added pseudocode for an authentication implementation

--- src/routes/HomeAPI.py
+++ src/routes/HomeAPI.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+# from ... import auth_token
 import requests
 
 # API for handling requests in relation to home data
@@ -18,17 +19,21 @@ def hello_world():
 # Takes the home address and zipcode as parameters
 # Assumes that the home address is properly formatted when sent over the request
 @home_api.route(HOME_API_BASE + "/septic", methods=["GET"])
+# @auth_token - Once implemented, we can wrap a given endpoint with the auth validation
 def has_septic_system():
-    address = request.form.get('address')
-    zip_code = request.form.get('zipcode')
-    PARAMS = { "address": address, "zipcode": zip_code }
-    # Assuming that we get a professional license to access the API
-    # we would pass the authentication token into the request
-    req = requests.get(url = CANARY_API_BASE + "property/details", params = PARAMS)
-    data = req.json()
-    sewer = data["sewer"]
-    septic_system = True if sewer == "Septic" else False
-    return septic_system
+    address = request.args.get('address')
+    zip_code = request.args.get('zipcode')
+    if address and zip_code:
+        PARAMS = { "address": address, "zipcode": zip_code }
+        # Assuming that we get a professional license to access the API
+        # we would pass the authentication token into the request
+        req = requests.get(url = CANARY_API_BASE + "property/details", params = PARAMS)
+        data = req.json()
+        sewer = data["sewer"]
+        septic_system = True if sewer == "Septic" else False
+        return septic_system
+    else:
+        return "Missing parameters", 400
 
 # This endpoint tests the Home API's ability to call an endpoint, 
 # retrieve data, and process a response. I used a free public API
@@ -36,9 +41,13 @@ def has_septic_system():
 # license for the House Canary API at the moment.
 @home_api.route(HOME_API_BASE + "/test", methods=["GET"])
 def test_endpoint_call():
-    post_id = request.form.get('post')
-    PARAMS = { "postId": post_id }
-    req = requests.get(url = "https://jsonplaceholder.typicode.com/comments", params = PARAMS)
-    data = req.json()
-    test_post = data[0]
-    return test_post
+    post_id = request.args.get('post')
+    if post_id:
+        PARAMS = { "postId": post_id }
+        req = requests.get(url = "https://jsonplaceholder.typicode.com/comments", params = PARAMS)
+        data = req.json()
+        # Simply return the first instance
+        test_post = data[0]
+        return test_post
+    else:
+        return "Missing parameters", 400
