829c64961562ee3447b3683b78a91d25f517bfe1
Miles Black <mablack15@gmail.com>
2021-12-05 15:30:52 -0500

    Added more comments and a README setup

--- src/routes/HomeAPI.py
+++ src/routes/HomeAPI.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
 
+# API for handling requests in relation to home data
+# Utilitzes home data from the house-canary API
+
 home_api = Blueprint('home_api', __name__)
 
 @home_api.route("/")
